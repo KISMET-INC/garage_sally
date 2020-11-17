e915bae429a958e08c6475a759a93e729ec9ed9a
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-11-17 15:59:45 -0800

    commit to push for team

--- client/src/views/Detail.js
+++ client/src/views/Detail.js
@@ -11,9 +11,9 @@ const Detail = props => {
         <div>
             <Navbar />
             <h1>Saturday, July 19, 1980</h1>
-            <h2>12233 Garrison Drive <button>Get Directions</button> </h2>
+            <h2>12233 Garrison Drive <button>Get Directions</button>  <button>Rate This Sale</button> </h2>
             <p> Visitors: 13  <button>CheckIn</button> </p>
-            <p> Hosted by: DealLover99 <button>Rate This Host</button> </p>
+            <p> Hosted by: DealLover99 </p>
 
             <button>Share this Link</button>
             <button>Add to Favorites</button>
