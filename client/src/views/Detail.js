d5f31975d7a609e2705b958c7854e9587fdd15cf
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-11-17 15:08:40 -0800

    small change

--- client/src/views/Detail.js
+++ client/src/views/Detail.js
@@ -10,13 +10,11 @@ const Detail = props => {
     return (
         <div>
             <Navbar />
-
-            <h2>12233 Garrison Drive <button>Get Directions</button> </h2>
             <h1>Saturday, July 19, 1980</h1>
-            <p>Sale id: {props.sale_id}</p>
+            <h2>12233 Garrison Drive <button>Get Directions</button> </h2>
             <p> Visitors: 13  <button>CheckIn</button> </p>
             <p> Hosted by: DealLover99 <button>Rate This Host</button> </p>
-            
+
             <button>Share this Link</button>
             <button>Add to Favorites</button>
             
